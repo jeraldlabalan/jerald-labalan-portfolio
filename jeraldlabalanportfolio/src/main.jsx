a6f458c733d9e48649f5c7f18aec19b6007aa960
Jerald Labalan <jeraldbalan26@gmail.com>
2024-11-09 13:50:56 +0800

    ulit

--- jeraldlabalanportfolio/src/main.jsx
+++ jeraldlabalanportfolio/src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
 import './index.css'
 import React from 'react';
@@ -7,9 +7,9 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from './common/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
